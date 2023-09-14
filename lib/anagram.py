f88313979c936f4c007334a934cf878817bc6560
Andy Patari <andy.patari@gmail.com>
2023-09-14 19:48:46 -0400

    passing tests

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, word_list):
+        res = []
+        for str in word_list:
+            if set(str) == set(self.word):
+                res.append(str)
+        return res
